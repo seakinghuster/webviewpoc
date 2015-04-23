e85d8bca8f03070f9eed5948b58e5a6cc268eb52
Timo Würsch <twuersch@gmail.com>
2015-04-23 22:19:27 +0200

    Integrated WebView.

--- src/main/java/com/example/webviewpoc/WebviewPoCActivity.java
+++ src/main/java/com/example/webviewpoc/WebviewPoCActivity.java
@@ -1,15 +1,51 @@
 package com.example.webviewpoc;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.webkit.WebView;
+import java.io.InputStream;
+import java.util.Scanner;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 
-public class WebviewPoCActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class WebviewPoCActivity extends Activity {
+  
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    
+    // Some intro.
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    
+    // Read HTML file from resources
+    Resources resources = getResources();
+    InputStream inputStream = resources.openRawResource(R.raw.test);
+    Scanner scanner = new Scanner(inputStream, "utf-8").useDelimiter("\\A");
+    String html = scanner.hasNext() ? scanner.next() : "";
+    
+    // Clean the HTML
+    Whitelist whitelist = Whitelist
+    .relaxed()
+    .addTags(
+      "header",
+      "nav",
+      "section",
+      "article",
+      "aside",
+      "footer",
+      "form",
+      "input",
+      "fieldset",
+      "title",
+      "time",
+      "noscript"
+    );
+    html = Jsoup.clean(html, whitelist);
+    
+    // Display in web view
+    WebView webView = (WebView) findViewById(R.id.webview);
+    webView.setVerticalScrollBarEnabled(true);
+    webView.loadData(html, "text/html", "utf-8");
+  }
 }
